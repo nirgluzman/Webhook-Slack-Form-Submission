564f294288acc4a2a6cbcc3b22931d379fc33bbb
nirgluzman <nir.gluzman@gmail.com>
2024-02-27 12:51:36 +0100

    add submit button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ function App() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
-  const handleSubmitForm = e => {};
+  const handleSubmitForm = e => {
+    e.preventDefault();
+
+    console.log('Form submitted:', { name, email, message });
+
+    // Reset form fields
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
 
   return (
     <div className='flex'>
@@ -24,6 +33,7 @@ function App() {
             value={name}
             onChange={e => setName(e.target.value)}
           />
+
           <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>
             Email
           </label>
@@ -35,17 +45,24 @@ function App() {
             value={email}
             onChange={e => setEmail(e.target.value)}
           />
+
           <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='message'>
             Message
           </label>
-          <input
+          <textarea
             className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
             id='message'
-            type='text'
-            placeholder='Enter you message here'
+            type='message'
+            placeholder='Write your message here'
             value={message}
             onChange={e => setMessage(e.target.value)}
           />
+
+          <button
+            className='mt-4 shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
+            onClick={e => handleSubmitForm(e)}>
+            Submit
+          </button>
         </form>
       </div>
     </div>
