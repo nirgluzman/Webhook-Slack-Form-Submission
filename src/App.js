1e739ce173df61dbe621ddff04d2fd5fe8184a83
nirgluzman <nir.gluzman@gmail.com>
2024-02-27 12:39:59 +0100

    add Tailwindcss and basic form entry

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmitForm = e => {};
+
   return (
-    <div className='App'>
-      <h1>Hello React</h1>
+    <div className='flex'>
+      <div className='w-1/2 container mx-auto mt-5'>
+        <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 self-center'>
+          <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>
+            Name
+          </label>
+          <input
+            className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+            id='name'
+            type='text'
+            placeholder='Max Müller'
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </form>
+      </div>
     </div>
   );
 }
